8d2fd6b9ea6cea5af9a151bcb6a7195e3ee8e081
hatim.hitman47@gmail.com <v-hadah@microsoft.com>
2019-08-01 21:28:21 +0530

    updated images

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
 
-$(document).ready(function(){
-	"use strict";
+$(document).ready(function () {
+  "use strict";
 
-	var window_width 	 = $(window).width(),
-	window_height 		 = window.innerHeight,
-	header_height 		 = $(".default-header").height(),
-	header_height_static = $(".site-header.static").outerHeight(),
-	fitscreen 			 = window_height - header_height;
+  var window_width = $(window).width(),
+    window_height = window.innerHeight,
+    header_height = $(".default-header").height(),
+    header_height_static = $(".site-header.static").outerHeight(),
+    fitscreen = window_height - header_height;
 
 
-	$(".fullscreen").css("height", window_height)
-	$(".fitscreen").css("height", fitscreen);
+  $(".fullscreen").css("height", window_height)
+  $(".fitscreen").css("height", fitscreen);
 
   //-------- Active Sticky Js ----------//
-     $(".default-header").sticky({topSpacing:0});
-  
+  $(".default-header").sticky({ topSpacing: 0 });
 
-     
-   // -------   Active Mobile Menu-----//
 
-  $(".menu-bar").on('click', function(e){
-      e.preventDefault();
-      $("nav").toggleClass('hide');
-      $("span", this).toggleClass("lnr-menu lnr-cross");
-      $(".main-menu").addClass('mobile-menu');
+
+  // -------   Active Mobile Menu-----//
+
+  $(".menu-bar").on('click', function (e) {
+    e.preventDefault();
+    $("nav").toggleClass('hide');
+    $("span", this).toggleClass("lnr-menu lnr-cross");
+    $(".main-menu").addClass('mobile-menu');
   });
 
 
@@ -36,11 +36,11 @@ $(document).ready(function(){
     // Remove links that don't actually link to anything
     .not('[href="#"]')
     .not('[href="#0"]')
-    .click(function(event) {
+    .click(function (event) {
       // On-page links
       if (
-        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-        && 
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        &&
         location.hostname == this.hostname
       ) {
         // Figure out element to scroll to
@@ -52,7 +52,7 @@ $(document).ready(function(){
           event.preventDefault();
           $('html, body').animate({
             scrollTop: target.offset().top
-          }, 1000, function() {
+          }, 1000, function () {
             // Callback after animation
             // Must change focus!
             var $target = $(target);
@@ -60,7 +60,7 @@ $(document).ready(function(){
             if ($target.is(":focus")) { // Checking if the target was focused
               return false;
             } else {
-              $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+              $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
               $target.focus(); // Set focus again
             };
           });
@@ -69,61 +69,73 @@ $(document).ready(function(){
     });
 
 
-    $(document).ready(function() {
-        $('#mc_embed_signup').find('form').ajaxChimp();
-    });      
-  
-var unavailableDates = [
-{start: '2019-08-3', end: '2019-08-4'}
-];
+  $(document).ready(function () {
+    $('#mc_embed_signup').find('form').ajaxChimp();
+  });
 
-$('#calendar').availabilityCalendar(unavailableDates);
+  var unavailableDates = [
+    { start: '2019-08-3', end: '2019-08-4' }
+  ];
+
+  $('#calendar').availabilityCalendar(unavailableDates);
 
 
-$(".fancybox").fancybox({
-  openEffect: "none",
-  closeEffect: "none"
+  $(".fancybox").fancybox({
+    openEffect: "none",
+    closeEffect: "none"
+  });
+
+  $(".zoom").hover(function () {
+
+    $(this).addClass('transition');
+  }, function () {
+    $(this).removeClass('transition');
+  });
+
+  // Open menu on default
+  $(".menu-bar").click();
+
+  // Bind all images from folder
+  var dir = "img/gallery/";
+  var fileextension = ".jpg";
+  var allslides = "";
+  var imageslide = '<div class="col-lg-3 col-md-4 col-xs-6 thumb">'
+    + '<a href="@path" class="fancybox" rel="ligthbox">'
+    + '<img src="@path" class="zoom img-fluid " alt="">'
+    + '</a>'
+    + '</div>'
+    for(var iCount = 1; iCount<=18;iCount++){
+      allslides += imageslide.replace(new RegExp("@path", 'g'), dir + iCount + fileextension);
+    }
+    $("#imageContainer").append(allslides);
 });
 
-$(".zoom").hover(function(){
+var marker;
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('google-map'), {
+    zoom: 19,
+    center: { lat: 19.192556, lng: 72.858073 }
+  });
+  marker = new google.maps.Marker({
+    map: map,
+    draggable: true,
+    animation: google.maps.Animation.DROP,
+    position: { lat: 19.192556, lng: 72.858073 }
+  });
+  marker.addListener('click', toggleBounce);
+  var contentString = "<div class='address'><strong>Anjuman-E-Ezzy Malad (East)<br> - Dawoodi Bohra Jamaat</strong></div>";
+  google.maps.event.addListener(marker, 'click', function () {
+    // Add this line
+    var infowindow = new google.maps.InfoWindow();
+    infowindow.setContent(contentString);
+    infowindow.open(map, marker);
+  });
+}
 
-$(this).addClass('transition');
-}, function(){  
-$(this).removeClass('transition');
-});
-
-// Open menu on default
-$(".menu-bar").click();
-
-
- });
-
- var marker;
- function initMap() {
-     var map = new google.maps.Map(document.getElementById('google-map'), {
-         zoom: 19,
-         center: { lat: 19.192556, lng: 72.858073 }
-     });      
-     marker = new google.maps.Marker({
-         map: map,
-         draggable: true,
-         animation: google.maps.Animation.DROP,
-         position: { lat: 19.192556, lng: 72.858073 }
-     });
-     marker.addListener('click', toggleBounce);
-     var contentString = "<div class='address'><strong>Anjuman-E-Ezzy Malad (East)<br> - Dawoodi Bohra Jamaat</strong></div>";
-     google.maps.event.addListener(marker, 'click', function () {
-         // Add this line
-         var infowindow = new google.maps.InfoWindow();
-         infowindow.setContent(contentString);
-         infowindow.open(map, marker);
-     });
- }
- 
- function toggleBounce() {
-     if (marker.getAnimation() !== null) {
-         marker.setAnimation(null);
-     } else {
-         marker.setAnimation(google.maps.Animation.BOUNCE);
-     }
- }
+function toggleBounce() {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}
